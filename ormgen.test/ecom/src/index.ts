9603ec9ad3858ca860d594f9fd258bcc1774ff4b
SchlagerKhan <kh.hansson@gmail.com>
2024-03-18 23:06:17 +0100

    Moved some tests

--- ormgen.test/ecom/src/index.ts
+++ ormgen.test/ecom/src/index.ts
@@ -2,11 +2,13 @@ import { run, PrismaGenerator } from 'ormgen';
 
 run({
 	search: {
-		root: 'ecom',
+		root: 'ecom/src',
 	},
 
 	generators: [
 		new PrismaGenerator({
+			schemaPath: 'ecom/prisma/schema.prisma',
+
 			datasource: {
 				provider: '"postgresql"',
 				url: 'env("DATABASE_URL")',
